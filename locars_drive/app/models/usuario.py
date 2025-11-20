5fe7be3c5d11eefdc96290c2faae022860c7548f
Almircesar8 <cesar25silvafilho@gmail.com>
2025-11-19 21:32:54 -0300

    Update usuario.py

--- locars_drive/app/models/usuario.py
+++ locars_drive/app/models/usuario.py
@@ -9,23 +9,22 @@ class Usuario(db.Model):
     __tablename__ = 'Usuario_'  # Nome da tabela no banco de dados
 
     id_usuario = db.Column(db.Integer, primary_key=True, autoincrement=True)  # id_Usuario
-    id_cliente = db.Column(db.Integer, nullable=False)      # id_Cliente
-    id_funcionario = db.Column(db.Integer, nullable=False)  # id_Funcionario
-    id_admin = db.Column(db.Integer, nullable=False)        # id_Admin
+    id_cliente = db.Column(db.Integer, nullable=False)                        # id_Cliente
+    id_funcionario = db.Column(db.Integer, nullable=False)                    # id_Funcionario
+    id_admin = db.Column(db.Integer, nullable=False)                          # id_Admin
     email = db.Column(db.String(255), nullable=False, unique=True)            # Email único
     nome_completo = db.Column(db.String(255))                                 # Nome completo
     senha = db.Column(db.String(255), nullable=False)                         # Senha
     data_nasc = db.Column(db.Date, nullable=False)                            # Data de nascimento
     cpf = db.Column(db.String(11), nullable=False, unique=True)               # CPF único
-    cnh = db.Column(db.String(11), nullable=True, unique=True)               # CNH única
+    cnh = db.Column(db.String(11), nullable=True, unique=True)                # CNH única
     cargo = db.Column(db.String(255), nullable=False)                         # Cargo
     salario = db.Column(db.Numeric(10, 2), nullable=False, default=0.00)      # Salário
     fk_funcao_id_funcao = db.Column(db.Integer)                               # Chave estrangeira para função
     fk_cidade_id_cidade = db.Column(db.Integer)                               # Chave estrangeira para cidade
-       # NOVO CAMPO: Data de criação, preenchido automaticamente
     data_criacao = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     
-    # Adicione outros campos do seu modelo aqui (ex: tipo_usuario_id, etc.)
+
 
     # Métodos para gestão de senhas (boa prática)
     def set_password(self, password):
@@ -49,3 +48,17 @@ class Usuario(db.Model):
     def get_id(self):
         return str(self.id_usuario)
     
+        return f"<Usuario id={self.id_usuario} email={self.email} nome={self.nome_completo} cargo={self.cargo}>"
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    @staticmethod
+    def get_by_id(id_):
+        return Usuario.query.get(id_)
+    @staticmethod
+    def get_all():
+        return Usuario.query.all() 
